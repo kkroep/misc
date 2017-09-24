e74bab16fcddac2629c80e0774fef43aaa0a7d92
Kees Kroep <k.kroep1@gmail.com>
2017-09-25 00:10:55 +0200

    stuff

--- testbenchCollection/python3/plotter.m
+++ testbenchCollection/python3/plotter.m
@@ -1,6 +1,8 @@
 %pkg load signal
 
 file1 = "outputFile1.txt";
+file2 = "outputFile2.txt";
+file3 = "outputFile3.txt";
 
 
 colorspec = {[0.4 0 0.8]; [0.4 0.8 0]; [0.4 0.7 0.7]; ...
@@ -14,19 +16,28 @@ colorspec_light = {[0.5 0.2 1]; [0.5 1 0.2]; [0.5 0.9 0.9]; ...
   [0.2 0.2 0.9]; [0.2 0.9 0.2]; [0.9 0.2 0.2]};
 
 graphics_toolkit gnuplot;
-%hold on;
+hold on;
 
 C1 = csvread(file1);
-size(C1)
+C2 = csvread(file2);
+C3 = csvread(file3);
 
 
-loglog(C1(:,1) , C1(:,2),  'LineWidth', 3 , 'Color', colorspec_light{mod(1,12)+1});
+plot(C1(:,1) , C1(:,2),  'LineWidth', 3 , 'Color', colorspec_light{mod(1,12)+1});
+plot(C2(:,1) , C2(:,2),  'LineWidth', 3 , 'Color', colorspec_light{mod(1,12)+2});
+plot(C3(:,1) , C3(:,2),  'LineWidth', 3 , 'Color', colorspec_light{mod(1,12)+3});
+
+
+hold off;
 
 %axis([C2(1,1) C2(end,1) min(min(C2))*1.1 max(max(C2))*1.1]);
+set(gca,'fontsize',14);
 xlabel('delay variance [%]');
-ylabel('components [-] ');
+ylabel('components*counts [-] ');
 legend(
-'50 ps',
+'\mu_c = 25  ps',
+'\mu_c = 50  ps',
+'\mu_c = 100 ps',
 'location', 'northwest');
 title('Requirements for 1 ps static accuracy');
 print('-dpdf', '-color', fullfile(pwd, 'plot.pdf'));
--- testbenchCollection/python3/simulation.py
+++ testbenchCollection/python3/simulation.py
@@ -4,12 +4,13 @@ import math
 
 outputFile1 = open("outputFile1.txt", "w")
 outputFile2 = open("outputFile2.txt", "w")
+outputFile3 = open("outputFile3.txt", "w")
 
 
-d_c = 50*10**-12 # delay of component
+d_c = 25*10**-12 # delay of component
 
 # component difference
-for i in range(100):
+for i in range(50):
     var = i/100*d_c # variance of signal
     N = var**2/(10**-12)**2 # var_tot = var/sqrt(N)
     N = math.ceil(N)
@@ -17,12 +18,34 @@ for i in range(100):
         outputFile1.write(str(i)+", "+str(N)+"\n")    
 
 
-# time difference
-for i in range(100):
+d_c = 50*10**-12 # delay of component
+
+# component difference
+for i in range(50):
     var = i/100*d_c # variance of signal
     N = var**2/(10**-12)**2 # var_tot = var/sqrt(N)
     N = math.ceil(N)
-    outputFile2.write(str(i)+", "+str(N)+"\n")    
+    if(N>0):
+        outputFile2.write(str(i)+", "+str(N)+"\n")    
+
+d_c = 100*10**-12 # delay of component
+
+
+for i in range(50):
+    var = i/100*d_c # variance of signal
+    N = var**2/(10**-12)**2 # var_tot = var/sqrt(N)
+    N = math.ceil(N)
+    if(N>0):
+        outputFile3.write(str(i)+", "+str(N)+"\n")    
+
+
+
+# time difference
+#for i in range(100):
+#    var = i/100*d_c # variance of signal
+#    N = var**2/(10**-12)**2 # var_tot = var/sqrt(N)
+#    N = math.ceil(N)
+#    outputFile2.write(str(i)+", "+str(N)+"\n")    
 
 
 
